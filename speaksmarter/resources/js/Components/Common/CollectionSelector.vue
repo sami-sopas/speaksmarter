6d8229e96f9ed69dce7dd2e9323ee2c0d1129895
Samuel Cuenca <ramirezcuenca@outlook.es>
2024-05-05 12:16:17 -0600

    defining emits, events for categories collection selector

--- speaksmarter/resources/js/Components/Common/CollectionSelector.vue
+++ speaksmarter/resources/js/Components/Common/CollectionSelector.vue
@@ -10,30 +10,62 @@
     })
 
     //Aqui se guardan los elementos seleccionados
-    const currentSelection = ref(1);
+    const currentSelection = ref(1); //Aqui se guardara la collecion del obj, no solo el nombre o id
     const selection = ref([]);
+    const emit = defineEmits(['onCategories']);
 
     const handleAddToSelection = () => {
-        selection.value.push(currentSelection);
+        //Comprobar si ya existe el elemento en la seleccion, para evitar duplicados
+        let alreadyExists = false;
+
+        selection.value.forEach(item => {
+            if (item.id === currentSelection.value.id) {
+                alreadyExists = true;
+                return;
+            }
+        });
+
+        if (alreadyExists) {
+            return;
+        }
+
+        selection.value.push(currentSelection.value);
+
+        //Emitir evento para enviar la seleccion al componente padre
+        emit('onCategories', selection.value);
+    }
+
+    const handleRemoveSelection = (index) => {
+        selection.value = selection.value.filter(item => item.id !== index)
+
+        //Emitir evento para enviar la seleccion al componente padre
+        emit('onCategories', selection.value);
     }
 
 </script>
 
 <template>
-    <select v-model="currentSelection">
-        <option v-for="(item, index) in collection" :key="index">
-            {{ item?.name }}
-        </option>
-    </select>
+    <div class="w-full">
+        <div class="flex">
+            <select v-model="currentSelection" class="w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+                <option v-for="(item, index) in collection" :key="index" :value="item">
+                    {{ item?.name }}
+                </option>
+            </select>
 
-    <button @click="handleAddToSelection">Add</button>
+            <button @click.prevent="handleAddToSelection" class="py-2 px-4 ml-1 text-white bg-indigo-500 hover:bg-indigo-700 rounded">
+                Add
+            </button>
+        </div>
 
-    <!-- Elementos seleccionados-->
-    <div>
-        <ul>
-            <li v-for="(item, index) in selection" :key="index">
-                {{ item }}
-            </li>
-        </ul>
+        <!-- Elementos seleccionados-->
+        <div>
+            <ul>
+                <li v-for="(item, index) in selection" :key="'index-' + index" @click.prevent="handleRemoveSelection(item.id)" class="mt-1 ml-1 mb-1 py-2 px-4 text-white bg-indigo-500 hover:bg-indigo-700 rounded" >
+                    {{ item.name }}
+                    <span class="float-right cursor-pointer">X</span>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
--- speaksmarter/resources/js/Components/Lessons/Form.vue
+++ speaksmarter/resources/js/Components/Lessons/Form.vue
@@ -13,6 +13,7 @@
     import SecondaryButton from '@/Components/SecondaryButton.vue';
     import TextInput from '@/Components/TextInput.vue';
     import CollectionSelector from '@/Components/Common/CollectionSelector.vue';
+    import { ref} from 'vue';
 
 
     defineProps({
@@ -36,6 +37,14 @@
         }
     })
 
+    //Guardar todas las categorieas
+    const categoriesSelected = ref([]);
+
+    //Evento en CollectionSelector para guardar las categorias seleccionadas
+    const onCategories = (_categories) => {
+        categoriesSelected.value = _categories;
+    }
+
     //Se emitira un evento submit, para ejecutar una peticion
     defineEmits(['submit'])
 </script>
@@ -55,30 +64,44 @@
                 <InputLabel for="name" value="name" />
                 <TextInput id="name" v-model="form.name" type="text" autocomplete="name" class="mt-1 block w-full" />
                 <InputError :message="$page.props.errorBags && $page.props.errorBags.default && $page.props.errorBags.default.name && $page.props.errorBags.default.name[0]"/> <!-- Mejorable ?-->
+                <br>
 
                 <InputLabel for="description" value="description" />
                 <TextInput id="description" v-model="form.description" type="text" autocomplete="description" class="mt-1 block w-full" />
                 <InputError :message="$page.props.errorBags && $page.props.errorBags.default && $page.props.errorBags.default.description && $page.props.errorBags.default.description[0]"/> <!-- Mejorable ?-->
+                <br>
 
                 <InputLabel for="content_uri" value="content uri" />
                 <TextInput id="content_uri" v-model="form.content_uri" type="text" autocomplete="content_uri" class="mt-1 block w-full" />
                 <InputError :message="$page.props.errorBags && $page.props.errorBags.default && $page.props.errorBags.default.content_uri && $page.props.errorBags.default.content_uri[0]"/> <!-- Mejorable ?-->
+                <br>
 
                 <InputLabel value="upload PDF" />
                 <SecondaryButton class="mt mr-2" type="button" >
                     Upload PDF
                 </SecondaryButton>
                 <InputError :message="$page.props.errorBags && $page.props.errorBags.default && $page.props.errorBags.default.pdf_uri && $page.props.errorBags.default.pdf_uri[0]"/> <!-- Mejorable ?-->
+                <br>
 
-                <InputLabel for="level" value="level" />
-                <select name="level" id="label">
-                    <option v-for="level in levels" :value="level.id" :key="'level-' + level.id">
-                        {{ level.name }}
-                    </option>
-                </select>
-                <InputError :message="$page.props.errorBags && $page.props.errorBags.default && $page.props.errorBags.default.level_id && $page.props.errorBags.default.level-id[0]"/> <!-- Mejorable ?-->
 
-                <CollectionSelector :collection="categories" />
+                <div class="w-full mt-5">
+                    <div class="flex">
+                        <div class="w-1/2 mr-1">
+                            <InputLabel for="level" value="level" />
+                            <select name="level" id="level" class="w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+                                <option v-for="level in levels" :value="level.id" :key="'level-' + level.id">
+                                    {{ level.name }}
+                                </option>
+                            </select>
+                            <InputError :message="$page.props.errorBags && $page.props.errorBags.default && $page.props.errorBags.default.level_id && $page.props.errorBags.default.level-id[0]"/> <!-- Mejorable ?-->
+                        </div>
+
+                        <div class="w-1/2 ml-1">
+                            <InputLabel for="categories" value="categories" />
+                            <CollectionSelector :collection="categories" name="categories" id="categories" @onCategories="onCategories"/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </template>
 
